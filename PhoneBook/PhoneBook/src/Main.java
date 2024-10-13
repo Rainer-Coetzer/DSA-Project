c68ec0920830bd3cd195e845d6fd3d2aff967eb9
RC <steve.dontwoneder@gmail.com>
2024-10-13 20:46:51 +0200

    Update Main.java

--- PhoneBook/PhoneBook/src/Main.java
+++ PhoneBook/PhoneBook/src/Main.java
@@ -1,9 +1,8 @@
-
-
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-    
+
     public static void main(String[] args) {
         Phonebook myphonebook = new Phonebook();
         Scanner sc = new Scanner(System.in);
@@ -11,12 +10,12 @@ public class Main {
 
         while (operation != -1) {
             try {
-                System.out.println("Input an operation (0 - Insert, 1 - Search, 2 - Delete, 3 - Display, 4 - Update, -1 to Exit):");
+                System.out.println("Input an operation (0 - Insert, 1 - Search, 2 - Delete, 3 - Display, 4 - Update, 5 - Group Contacts, 6 - Search Group, 7 - Remove Contact from Group, 8 - Insert into Group, 9 - Delete Group, -1 to Exit):");
                 operation = sc.nextInt();
                 sc.nextLine(); // Consume newline
 
                 switch (operation) {
-                    case 0:
+                    case 0: // Insert Contact
                         String name = getInputWithConfirmation(sc, "name");
                         if (name == null) break;
 
@@ -27,7 +26,7 @@ public class Main {
                         System.out.println("Contact inserted.");
                         break;
 
-                    case 1:
+                    case 1: // Search Contact
                         System.out.println("Give a name to search (or type '1' to cancel): ");
                         String search = sc.nextLine();
                         if (!search.equals("1")) {
@@ -40,7 +39,7 @@ public class Main {
                         }
                         break;
 
-                    case 2:
+                    case 2: // Delete Contact
                         System.out.println("Give a name to delete (or type '1' to cancel): ");
                         String delete = sc.nextLine();
                         if (!delete.equals("1")) {
@@ -54,12 +53,12 @@ public class Main {
                         }
                         break;
 
-                    case 3:
+                    case 3: // Display All Contacts
                         System.out.println("Displaying all contacts:");
                         myphonebook.displayAllContacts();
                         break;
 
-                    case 4:
+                    case 4: // Update Contact
                         System.out.println("Give the current name to update (or type '1' to cancel): ");
                         String currentName = sc.nextLine();
                         if (!currentName.equals("1")) {
@@ -73,7 +72,61 @@ public class Main {
                         }
                         break;
 
-                    case -1:
+                    case 5: // Group Contacts
+                        System.out.println("Enter a group name: ");
+                        String groupName = sc.nextLine();
+
+                        ArrayList<String> contactNames = new ArrayList<>();
+                        System.out.println("Enter contact names to add to the group (type 'Done' when finished):");
+                        while (true) {
+                            String contactName = sc.nextLine();
+                            if (contactName.equalsIgnoreCase("Done")) {
+                                break;
+                            }
+                            contactNames.add(contactName);
+                        }
+
+                        myphonebook.groupContacts(groupName, contactNames.toArray(new String[0]));
+                        System.out.println("Group '" + groupName + "' created.");
+                        break;
+
+                    case 6: // Search Contacts by Group
+                        System.out.println("Enter the group name to search: ");
+                        String searchGroup = sc.nextLine();
+                        myphonebook.searchContactsByGroup(searchGroup);
+                        break;
+
+                    case 7: // Remove Contact from Group
+                        System.out.println("Enter the group name: ");
+                        String groupToRemoveFrom = sc.nextLine();
+
+                        System.out.println("Enter the contact name to remove from the group: ");
+                        String contactToRemove = sc.nextLine();
+
+                        myphonebook.deleteFromGroup(contactToRemove, groupToRemoveFrom);
+                        System.out.println("Contact removed from group.");
+                        break;
+
+                    case 8: // Insert into Group
+                        System.out.println("Enter the group name: ");
+                        String existingGroupName = sc.nextLine();
+
+                        System.out.println("Enter the contact name to add to the group: ");
+                        String contactToInsert = sc.nextLine();
+
+                        myphonebook.insertIntoGroup(contactToInsert, existingGroupName);
+                        System.out.println("Contact added to group '" + existingGroupName + "'.");
+                        break;
+
+                    case 9: // Delete Group
+                        System.out.println("Enter the group name to delete: ");
+                        String groupToDelete = sc.nextLine();
+
+                        myphonebook.deleteGroup(groupToDelete);
+                        System.out.println("Group '" + groupToDelete + "' deleted.");
+                        break;
+
+                    case -1: // Exit
                         System.out.println("Exiting phonebook...");
                         break;
 
